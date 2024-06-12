d356fd2ab343059b1598bb5a80693b46657fe252
martinegs <martinegs2012@gmail.com>
2024-06-12 15:14:37 -0300

    Terminado falta css

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -2,7 +2,6 @@
 const res = await fetch(`https://reqres.in/api/users/${id}`);
 const data = await res.json();
 return data.data;
- 
 }
 async function UserPage({params}){
     const user = await getUser(params.id);
